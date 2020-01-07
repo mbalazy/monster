0538970ad1bf648db73e2b945d7778830b2eadb6
mbalazy <p@mbalazy.pl>
2020-01-07 22:05:58 +0100

    Updates

---
+++ precache-manifest.513f310802a1580d656487c7f6390a0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199371c79c66c6032d2e0bfffe5bd39b",
+    "url": "/mbalazy/monster/index.html"
+  },
+  {
+    "revision": "e046b51cdef0a9479012",
+    "url": "/mbalazy/monster/static/css/main.ab4ffd2f.chunk.css"
+  },
+  {
+    "revision": "26cb6f88ea39dd607e43",
+    "url": "/mbalazy/monster/static/js/2.7e7ae4cf.chunk.js"
+  },
+  {
+    "revision": "e046b51cdef0a9479012",
+    "url": "/mbalazy/monster/static/js/main.2e1f6232.chunk.js"
+  },
+  {
+    "revision": "0a36c25a76f28f96ee9f",
+    "url": "/mbalazy/monster/static/js/runtime-main.5ef1743e.js"
+  }
+]);
